807b1677d1cb0c3ee6098e7520ce0a242d9a0b1a
Naveen <naveen.cosinetech@gmail.com>
2023-09-08 08:37:09 +0530

    initoal commit

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("task");
+    const addTaskButton = document.getElementById("addTask");
+    const taskList = document.getElementById("taskList");
+
+    addTaskButton.addEventListener("click", function () {
+        const taskText = taskInput.value.trim();
+        if (taskText !== "") {
+            const li = document.createElement("li");
+            li.innerHTML = `
+                ${taskText}
+                <button class="delete">Delete</button>
+            `;
+            taskList.appendChild(li);
+            taskInput.value = "";
+
+            // Add event listener to delete button
+            const deleteButton = li.querySelector(".delete");
+            deleteButton.addEventListener("click", function () {
+                taskList.removeChild(li);
+            });
+        }
+    });
+
+    taskInput.addEventListener("keyup", function (event) {
+        if (event.key === "Enter") {
+            addTaskButton.click();
+        }
+    });
+});
